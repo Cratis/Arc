58fd956860c4e302954559d5dc585a0f5eb8e64a
Einar <einari@me.com>
2025-12-19 10:03:19 +0100

    Moving internals visible to into the project file

--- Source/DotNET/Arc/Assembly.cs
+++
@@ -1,9 +0,0 @@
-// Copyright (c) Cratis. All rights reserved.
-// Licensed under the MIT license. See LICENSE file in the project root for full license information.
-
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Cratis.Arc.EntityFrameworkCore")]
-[assembly: InternalsVisibleTo("Cratis.Arc.EntityFrameworkCore.Specs")]
-[assembly: InternalsVisibleTo("Cratis.Arc.MongoDB")]
-[assembly: InternalsVisibleTo("Cratis.Arc.Specs")]
