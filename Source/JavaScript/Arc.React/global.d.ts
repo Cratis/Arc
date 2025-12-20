e1be0dc9800196095a77439cde24d7187acce6bd
Einar <einari@me.com>
2025-12-20 23:43:28 +0100

    Specs setup working in VSCode

--- Source/JavaScript/Arc.React/global.d.ts
+++ Source/JavaScript/Arc.React/global.d.ts
@@ -1,11 +1,22 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+/// <reference types="vitest/globals" />
+/// <reference types="chai" />
+/// <reference types="sinon-chai" />
+
 import * as React from 'react';
 
 declare global {
   namespace JSX {
     interface Element extends React.ReactElement {}
     // Add additional JSX-related overrides if necessary.
+  }
+  
+  // Ensure chai should is available globally
+  namespace Chai {
+    interface Assertion extends LanguageChains, NumericComparison, TypeComparison {
+      called: Assertion;
+    }
   }
 }
