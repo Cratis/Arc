117e010f96afea6e73a35ad46c28df5ba30215af
Einar <einari@me.com>
2025-12-26 00:26:11 +0100

    Adding specs to verify the support for internal commands and queries

---
+++ Source/DotNET/Arc.Core.Specs/Commands/ModelBound/for_CommandAttributeExtensions/when_getting_handle_method_on_internal_type.cs
@@ -0,0 +1,16 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using System.Reflection;
+
+namespace Cratis.Arc.Commands.ModelBound.for_CommandAttributeExtensions;
+
+public class when_getting_handle_method_on_internal_type : Specification
+{
+    MethodInfo _result;
+
+    void Because() => _result = typeof(InternalCommand).GetHandleMethod();
+
+    [Fact] void should_get_handle_method() => _result.ShouldNotBeNull();
+    [Fact] void should_get_method_named_handle() => _result.Name.ShouldEqual("Handle");
+}
---
+++ Source/DotNET/Arc.Core.Specs/Commands/ModelBound/for_CommandHandlerProvider/when_initializing_with_public_command_with_internal_handle.cs
@@ -0,0 +1,21 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+namespace Cratis.Arc.Commands.ModelBound.for_CommandHandlerProvider;
+
+public class when_initializing_with_public_command_with_internal_handle : Specification
+{
+    CommandHandlerProvider _provider;
+    ITypes _types;
+
+    void Establish()
+    {
+        _types = Substitute.For<ITypes>();
+        _types.All.Returns([typeof(PublicCommandWithInternalHandle)]);
+    }
+
+    void Because() => _provider = new CommandHandlerProvider(_types);
+
+    [Fact] void should_have_one_handler() => _provider.Handlers.Count().ShouldEqual(1);
+    [Fact] void should_be_able_to_get_handler_for_command() => _provider.TryGetHandlerFor(new PublicCommandWithInternalHandle(), out _).ShouldBeTrue();
+}
