e1809f1df4307ed6bee48994df358c7275640b45
Einar <einari@me.com>
2026-02-18 16:49:47 +0100

    Revert "Format fix for header"
    This reverts commit 91882a28c37fe880a430882b5193647bee21b254.

--- Source/JavaScript/Arc.React/storybook-static/assets/preview-Du8Ond3e.js
+++
@@ -1,1 +0,0 @@
-const a={parameters:{backgrounds:{default:"dark",values:[{name:"dark",value:"#1a1a1a"},{name:"light",value:"#f5f5f5"}]},controls:{matchers:{color:/(background|color)$/i,date:/Date$/i}}}};export{a as default};
