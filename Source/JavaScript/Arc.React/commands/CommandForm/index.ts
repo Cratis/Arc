16ced7c4e936b3eb2f783d62fddbb3a5866c8afd
Einar <einari@me.com>
2026-02-19 14:38:37 +0100

    Adding missing CommandFormContext

--- Source/JavaScript/Arc.React/commands/CommandForm/index.ts
+++ Source/JavaScript/Arc.React/commands/CommandForm/index.ts
@@ -1,9 +1,10 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-export * from './CommandForm';
-export * from './CommandFormField';
-export * from './ValidationMessage';
-export * from './CommandFormFields';
 export * from './asCommandFormField';
+export * from './CommandForm';
+export * from './CommandFormContext';
+export * from './CommandFormField';
+export * from './CommandFormFields';
+export * from './ValidationMessage';
 export * from './fields';
