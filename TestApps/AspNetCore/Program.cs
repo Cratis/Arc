5c2bdd58c54fe6251147e1c078dd99f0716e5892
woksin <sindrewilting@gmail.com>
2026-02-26 00:30:57 +0100

    Enhance project configuration and command handling with new settings and Swagger integration

--- TestApps/AspNetCore/Program.cs
+++ TestApps/AspNetCore/Program.cs
@@ -1,10 +1,15 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Arc.Swagger;
+
 var builder = WebApplication.CreateBuilder(args);
 builder.AddCratisArc();
 builder.Services.AddControllers();
 builder.Services.AddMvc();
+
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen(options => options.AddConcepts());
 
 var app = builder.Build();
 app.UseRouting();
@@ -13,5 +18,8 @@ app.UseCratisArc();
 
 app.MapControllers();
 app.MapGet("/", () => "Hello World!");
+
+app.UseSwagger();
+app.UseSwaggerUI();
 
 await app.RunAsync();
