1d404c90e16fc871ac241c24576a9fd740821161
Einar <einari@me.com>
2026-03-09 21:52:38 +0100

    Fixing namespaces and types to be correct

--- Source/DotNET/Chronicle/ArcBuilderExtensions.cs
+++ Source/DotNET/Chronicle/ArcBuilderExtensions.cs
@@ -2,9 +2,9 @@
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
 using Cratis.Arc.Chronicle.Tenancy;
-using Cratis.Chronicle.AspNetCore;
-using Microsoft.AspNetCore.Builder;
+using Cratis.Chronicle;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
 
 namespace Cratis.Arc;
 
@@ -17,17 +17,17 @@ public static class ArcBuilderExtensions
     /// Adds Chronicle support to Arc.
     /// </summary>
     /// <param name="builder">The <see cref="IArcBuilder"/> to add to.</param>
-    /// <param name="configureOptions">Optional callback for configuring <see cref="ChronicleAspNetCoreOptions"/>.</param>
+    /// <param name="configureOptions">Optional callback for configuring <see cref="ChronicleClientOptions"/>.</param>
     /// <param name="configureChronicleBuilder">Optional callback for configuring the <see cref="IChronicleBuilder"/>.</param>
     /// <returns><see cref="IArcBuilder"/> for continuation.</returns>
     public static IArcBuilder WithChronicle(
         this IArcBuilder builder,
-        Action<ChronicleAspNetCoreOptions>? configureOptions = default,
+        Action<ChronicleClientOptions>? configureOptions = default,
         Action<IChronicleBuilder>? configureChronicleBuilder = default)
     {
         builder.Services.AddAggregateRoots(builder.Types);
 
-        ((WebApplicationBuilder)builder.AppBuilder).AddCratisChronicle(
+        builder.AppBuilder.AddCratisChronicle(
             configureOptions: options =>
             {
                 options.EventStoreNamespaceResolverType = typeof(TenantNamespaceResolver);
