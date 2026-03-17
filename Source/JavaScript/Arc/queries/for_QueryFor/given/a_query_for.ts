846e8f3e2c4dc1badcb6953213c3dcdce9190896
Einar <einari@me.com>
2026-03-17 21:18:36 +0100

    Add base query specs for partially missing required args

--- Source/JavaScript/Arc/queries/for_QueryFor/given/a_query_for.ts
+++ Source/JavaScript/Arc/queries/for_QueryFor/given/a_query_for.ts
@@ -1,18 +1,26 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
-import { TestQueryFor, TestEnumerableQueryFor, TestQueryForWithoutRequiredParams, TestQueryForWithParameterDescriptorValues } from './TestQueries';
+import {
+    TestQueryFor,
+    TestEnumerableQueryFor,
+    TestQueryForWithoutRequiredParams,
+    TestQueryForWithParameterDescriptorValues,
+    TestQueryForWithMultipleRequiredParameters
+} from './TestQueries';
 
 export class a_query_for {
     query: TestQueryFor;
     enumerableQuery: TestEnumerableQueryFor;
     queryWithoutParams: TestQueryForWithoutRequiredParams;
     queryWithParameterDescriptorValues: TestQueryForWithParameterDescriptorValues;
+    queryWithMultipleRequiredParameters: TestQueryForWithMultipleRequiredParameters;
 
     constructor() {
         this.query = new TestQueryFor();
         this.enumerableQuery = new TestEnumerableQueryFor();
         this.queryWithoutParams = new TestQueryForWithoutRequiredParams();
         this.queryWithParameterDescriptorValues = new TestQueryForWithParameterDescriptorValues();
+        this.queryWithMultipleRequiredParameters = new TestQueryForWithMultipleRequiredParameters();
     }
 }
