2d92bc4e6c770b3bc971d365029b5f02ed7ff3d4
Einar <einari@me.com>
2026-04-14 20:09:16 +0200

    Restore Arc.Core.Generators.Specs project

---
+++ Source/DotNET/Arc.Core.Generators.Specs/for_QueryMetadataGenerator/when_generating_without_read_models/and_class_without_read_model_attribute_exists.cs
@@ -0,0 +1,31 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Arc.Generators.Specs.Testing;
+using Microsoft.CodeAnalysis;
+
+namespace Cratis.Arc.Generators.for_QueryMetadataGenerator.when_generating_without_read_models;
+
+public class and_class_without_read_model_attribute_exists : Specification
+{
+    GeneratorDriverRunResult _result;
+    string _generatedSource;
+
+    void Because()
+    {
+        _result = GeneratorTestHelper.RunGenerator("""
+            namespace TestApp;
+
+            public class NotAReadModel
+            {
+                public static NotAReadModel GetAll() => new();
+            }
+            """);
+
+        _generatedSource = GeneratorTestHelper.GetGeneratedSourceByHintName(_result, "CratisArcGeneratedMarker.g.cs");
+    }
+
+    [Fact] void should_generate_only_marker_source() => _result.GeneratedTrees.Length.ShouldEqual(1);
+    [Fact] void should_generate_marker_type() => _generatedSource.ShouldContain("public static class GeneratedMarker");
+    [Fact] void should_have_no_diagnostics() => _result.Diagnostics.Length.ShouldEqual(0);
+}
