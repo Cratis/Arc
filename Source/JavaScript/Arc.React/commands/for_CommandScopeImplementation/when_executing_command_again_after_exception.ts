ac97c98ec63af8251106289843ad39985a49a905
Einar <einari@me.com>
2026-04-24 14:43:36 +0200

    Add specs for CommandScope validation failures, exceptions, and nesting
    - when_executing_command_with_validation_failure: failures stored per command
    - when_executing_command_with_exception: exception messages stored per command
    - when_executing_command_again_after_validation_failure: cleared on re-execution
    - when_executing_command_again_after_exception: cleared on re-execution
    - when_child_scope_has_validation_failure: parent aggregates child failures
    - when_child_scope_has_exception: parent aggregates child exceptions
    - when_command_registers_to_nearest_scope: commands bind to nearest scope only
    - when_parent_and_child_scopes_have_validation_failures: each scope owns its own

---
+++ Source/JavaScript/Arc.React/commands/for_CommandScopeImplementation/when_executing_command_again_after_exception.ts
@@ -0,0 +1,37 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import sinon from 'sinon';
+import { CommandResult } from '@cratis/arc/commands';
+import { CommandScopeImplementation } from '../CommandScopeImplementation';
+import { FakeCommand } from './FakeCommand';
+
+describe('when executing command again after exception', async () => {
+    const exceptionResult = new CommandResult({
+        correlationId: '00000000-0000-0000-0000-000000000000',
+        isSuccess: false,
+        isAuthorized: true,
+        isValid: true,
+        hasExceptions: true,
+        validationResults: [],
+        exceptionMessages: ['Something went wrong'],
+        exceptionStackTrace: 'Stack trace here',
+        authorizationFailureReason: '',
+        response: {}
+    }, Object, false);
+
+    const scope = new CommandScopeImplementation(() => {});
+    const command = new FakeCommand(true, exceptionResult);
+    scope.addCommand(command);
+
+    await scope.execute();
+
+    // Re-execute: command reports hasChanges again and succeeds this time
+    command['_hasChanges'] = true;
+    command.execute = sinon.fake(() => Promise.resolve(CommandResult.empty));
+
+    await scope.execute();
+
+    it('should not have exceptions after successful re-execution', () => scope.hasExceptions.should.be.false);
+    it('should not track exceptions for the command', () => scope.exceptions.has(command).should.be.false);
+});
---
+++ Source/JavaScript/Arc.React/commands/for_CommandScopeImplementation/when_parent_and_child_scopes_have_validation_failures.ts
@@ -0,0 +1,51 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import { CommandResult } from '@cratis/arc/commands';
+import { CommandScopeImplementation } from '../CommandScopeImplementation';
+import { FakeCommand } from './FakeCommand';
+
+describe('when both parent and child scopes have their own validation failures', async () => {
+    const parentFailedResult = new CommandResult({
+        correlationId: '00000000-0000-0000-0000-000000000000',
+        isSuccess: false,
+        isAuthorized: true,
+        isValid: false,
+        hasExceptions: false,
+        validationResults: [{ severity: 0, message: 'Parent field required', members: ['parentField'], state: {} }],
+        exceptionMessages: [],
+        exceptionStackTrace: '',
+        authorizationFailureReason: '',
+        response: {}
+    }, Object, false);
+
+    const childFailedResult = new CommandResult({
+        correlationId: '00000000-0000-0000-0000-000000000001',
+        isSuccess: false,
+        isAuthorized: true,
+        isValid: false,
+        hasExceptions: false,
+        validationResults: [{ severity: 0, message: 'Child field required', members: ['childField'], state: {} }],
+        exceptionMessages: [],
+        exceptionStackTrace: '',
+        authorizationFailureReason: '',
+        response: {}
+    }, Object, false);
+
+    const parentScope = new CommandScopeImplementation(() => {});
+    const childScope = new CommandScopeImplementation(() => {}, undefined, parentScope);
+
+    const parentCommand = new FakeCommand(true, parentFailedResult);
+    const childCommand = new FakeCommand(true, childFailedResult);
+    parentScope.addCommand(parentCommand);
+    childScope.addCommand(childCommand);
+
+    await parentScope.execute();
+    await childScope.execute();
+
+    it('should report has validation failures on the parent scope', () => parentScope.hasValidationFailures.should.be.true);
+    it('should report has validation failures on the child scope', () => childScope.hasValidationFailures.should.be.true);
+    it('should track parent command validation failure in parent scope', () => parentScope.validationFailures.has(parentCommand).should.be.true);
+    it('should track child command validation failure in child scope', () => childScope.validationFailures.has(childCommand).should.be.true);
+    it('should not track child command in parent scope directly', () => parentScope.validationFailures.has(childCommand).should.be.false);
+});
