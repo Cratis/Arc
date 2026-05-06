27afe4d839c21f12355e91d4bae9f3a309861dbe
Einar <einari@me.com>
2026-05-06 12:59:07 +0200

    Fixing types

--- Source/DotNET/MongoDB.Specs/for_DerivedTypeDiscriminatorConvention/given/a_derived_type_discriminator_convention.cs
+++ Source/DotNET/MongoDB.Specs/for_DerivedTypeDiscriminatorConvention/given/a_derived_type_discriminator_convention.cs
@@ -7,7 +7,7 @@ namespace Cratis.Arc.MongoDB.for_DerivedTypeDiscriminatorConvention.given;
 
 public class a_derived_type_discriminator_convention : Specification
 {
-    protected static readonly Guid _derivedTypeIdentifier = new(0xb91c21cd, 0x56db, 0x4edc, 0x80, 0x5b, 0xb1, 0xd1, 0xff, 0x9a, 0xa7, 0x72);
+    protected static readonly string _derivedTypeIdentifier = "cf759496-8e30-4d1c-92a5-51c6b1dca299";
 
     protected IDerivedTypes _derivedTypes;
     protected DerivedTypeDiscriminatorConvention _convention;
