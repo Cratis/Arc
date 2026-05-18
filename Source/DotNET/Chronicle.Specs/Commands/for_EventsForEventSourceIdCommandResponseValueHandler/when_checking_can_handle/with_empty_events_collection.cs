913f3bc698215e8c58b6d0691fd1c2cb3fafc597
Einar <einari@me.com>
2026-05-18 13:58:05 +0200

    Switching to correct version of EventForEventSourceId - this was an oversight
    Co-authored-by: einari <134365+einari@users.noreply.github.com>

--- Source/DotNET/Chronicle.Specs/Commands/for_EventsForEventSourceIdCommandResponseValueHandler/when_checking_can_handle/with_empty_events_collection.cs
+++ Source/DotNET/Chronicle.Specs/Commands/for_EventsForEventSourceIdCommandResponseValueHandler/when_checking_can_handle/with_empty_events_collection.cs
@@ -1,6 +1,8 @@
 // Copyright (c) Cratis. All rights reserved.
 // Licensed under the MIT license. See LICENSE file in the project root for full license information.
 
+using Cratis.Chronicle.EventSequences;
+
 namespace Cratis.Arc.Chronicle.Commands.for_EventsForEventSourceIdCommandResponseValueHandler.when_checking_can_handle;
 
 public class with_empty_events_collection : given.an_events_for_event_source_id_command_response_value_handler
