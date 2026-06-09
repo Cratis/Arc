1c269b03eb6242daa9fd9f46dd7d51b5c4360a18
Einar <einari@me.com>
2026-06-09 14:41:57 +0200

    Update type metadata and tooling for geospatial types
    - Update ProxyGenerator TypeExtensions to recognize Point as known type
    - Promote single-outcome spec files for Point (formerly nested in folders)
    - Update package versions in Directory.Packages.props
    - Update Arc package.json version

---
+++ Source/DotNET/Tools/ProxyGenerator.Specs/for_TypeExtensions/when_checking_if_point_is_known_type.cs
@@ -0,0 +1,15 @@
+// Copyright (c) Cratis. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+using Cratis.Geospatial;
+
+namespace Cratis.Arc.ProxyGenerator.for_TypeExtensions;
+
+public class when_checking_if_point_is_known_type : Specification
+{
+    bool _result;
+
+    void Because() => _result = typeof(Point).IsKnownType();
+
+    [Fact] void should_be_a_known_type() => _result.ShouldBeTrue();
+}
