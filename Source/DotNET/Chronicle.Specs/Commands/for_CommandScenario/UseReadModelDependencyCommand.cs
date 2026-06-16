78b3f84f14d10b26e4be13be6806d45148f0f9e0
woksin <sindrewilting@gmail.com>
2026-06-16 10:41:44 +0200

    Add ProvideReadModelDependencyCommand and related tests

--- Source/DotNET/Chronicle.Specs/Commands/for_CommandScenario/UseReadModelDependencyCommand.cs
+++ Source/DotNET/Chronicle.Specs/Commands/for_CommandScenario/UseReadModelDependencyCommand.cs
@@ -16,8 +16,23 @@ public record UseReadModelDependencyCommand(EventSourceId EventSourceId)
         new(readModel.Balance);
 }
 
+[Command]
+public record ProvideReadModelDependencyCommand(EventSourceId EventSourceId)
+{
+    public ProvidedAccountBalance Provide(AccountBalanceReadModel readModel) =>
+        new(readModel.Balance);
+
+    public ReadModelDependencyProvided Handle(ProvidedAccountBalance balance) =>
+        new(balance.Value);
+}
+
+public record ProvidedAccountBalance(decimal Value);
+
 [ReadModel]
 public record AccountBalanceReadModel(decimal Balance);
 
 [EventType("de9f2e68-9a50-4e08-9c0f-3f7a2f6c47a1")]
 public record ReadModelDependencyUsed(decimal Balance);
+
+[EventType("510b5c70-5948-42cb-9ab7-a3ee9c649dcd")]
+public record ReadModelDependencyProvided(decimal Balance);
